89cd46b45ee22471541f57110e01d3ac93587d20
Maria Eduarda <mariaeduardabarbosah@gmail.com>
2025-02-28 13:27:12 -0300

    Adicionar Grace e seus comandos de sprite

--- scenes/game.js
+++ scenes/game.js
@@ -14,6 +14,7 @@ export class GameScene extends Phaser.Scene {
         this.load.image('paisagem', '../assets/paisagem.png');
         this.load.image('plataforma', '../assets/plataforma.png');
         this.load.image('personagem_frente', '../assets/personagem_frente.png');
+        this.load.spritesheet("grace_sprite", "../assets/spritesheetGrace.png", { frameWidth: 64, frameHeight: 64 })
     }
 
     create() { //Criar elementos da tela do jogo
@@ -22,12 +23,10 @@ export class GameScene extends Phaser.Scene {
         this.add.image(this.larguraJogo/2, this.alturaJogo/2, 'paisagem').setScale(0.6);
 
         //Adicionar sprite da personagem
-        this.load.spritesheet("grace_sprite", "..assets/spritesheetGrace.jpg", { frameWidth: 64, frameHeight: 64 })
-
-        //Adicionar player, dimensão e marcação de colisão
-        this.player = this.physics.add.sprite(this.larguraJogo/2, 100, 'personagem_frente').setScale(0.4);
-        this.player.body.setSize(151, 195, true);
+        this.player = this.physics.add.sprite(this.larguraJogo/2, 100, 'grace_sprite').setScale(1.3);
+        // this.player.body.setSize(151, 195, true);
         this.player.setCollideWorldBounds(true);
+
 
         //Adicionar plataformas 1 e 2, dimensão e marcação de colisão
         this.plataformas[0] = this.physics.add.staticImage(200, 450, 'plataforma');
@@ -46,17 +45,47 @@ export class GameScene extends Phaser.Scene {
         //Adicionar os controles do teclado
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        // Animações da personagem
+        this.anims.create({
+            key: 'direita',
+            frames: this.anims.generateFrameNumbers('grace_sprite', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'esquerda',
+            frames: this.anims.generateFrameNumbers('grace_sprite', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'parada',
+            frames: [{ key: 'grace_sprite', frame: 4 }],
+            frameRate: 20
+        });
+
         }
 
 
     update() {
 
-        if (this.cursors.left.isDown) { // Movimentação horizontal
+        if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160);
+            if (this.player.anims.currentAnim?.key !== 'esquerda') {
+                this.player.anims.play('esquerda', true);
+            }
         } else if (this.cursors.right.isDown) {
             this.player.setVelocityX(160);
+            if (this.player.anims.currentAnim?.key !== 'direita') {
+                this.player.anims.play('direita', true);
+            }
         } else {
             this.player.setVelocityX(0);
+            if (this.player.anims.currentAnim?.key !== 'parada') {
+                this.player.anims.play('parada', true);
+            }
         }
 
         // Lógica de pulo (vertical) 
