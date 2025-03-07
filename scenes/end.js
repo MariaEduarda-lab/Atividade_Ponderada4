eff407ab63c4a90ace76d430c6876abb995c9281
Maria Eduarda <mariaeduardabarbosah@gmail.com>
2025-03-07 08:20:52 -0300

    Ajustando cena final

--- scenes/end.js
+++ scenes/end.js
@@ -15,12 +15,17 @@ export class EndScene extends Phaser.Scene {
         this.load.image("ganhou", "../assets/ganhou.png");
         this.load.image("menu", "../assets/botao_menu.png");
         this.load.image("restart", "../assets/botao_restart.png");
+        this.load.image("fim", "../assets/fim.png");
+        this.load.image("tiao", "../assets/tiao.png");
+        this.load.image("ganhou", "../assets/ganhou.png");
     }
 
     create() {
-        this.add.image(this.larguraJogo/2, 205, "ganhou").setScale(0.5);
-        this.botaoMenu = this.add.image(this.larguraJogo/2 - 100, 320, "menu").setScale(0.7).setInteractive();
-        this.botaoRestart = this.add.image(this.larguraJogo/2 + 100, 320, "restart").setScale(0.7).setInteractive();
+        this.add.image(this.larguraJogo/2, this.alturaJogo/2, 'fim').setScale(1.3);
+        this.add.image(this.larguraJogo/2 - 200, this.alturaJogo/2 + 100, 'tiao').setScale(1);
+        this.add.image(300, 100, "ganhou").setScale(0.7);
+        this.botaoMenu = this.add.image(this.larguraJogo/2 + 50, 320, "menu").setScale(0.7).setInteractive();
+        this.botaoRestart = this.add.image(this.larguraJogo/2 + 230, 320, "restart").setScale(0.7).setInteractive();
 
         this.botaoMenu.on("pointerover", () => {
             this.input.setDefaultCursor("pointer");
